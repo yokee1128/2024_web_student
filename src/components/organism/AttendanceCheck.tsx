3f68698cec93dad7ee41839e1c5489f619dafbce
yonghyuny <lovelove4695@naver.com>
2024-07-13 15:39:58 +0900

    [KYJ]test save

--- src/components/organism/AttendanceCheck.tsx
+++ src/components/organism/AttendanceCheck.tsx
@@ -1,11 +1,19 @@
-import { Chip } from "@mui/material";
+import { Grid, MenuItem } from "@mui/material";
+
+
 
 const AttendanceCheck = () => {
+  
   return (
-    <div>
-      <Chip label="AttendanceCheck" />
-    </div>
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+  {Array.from(Array(6)).map((_, index) => (
+    <Grid item xs={2} sm={4} md={4} key={index}>
+      <MenuItem>xs=2</MenuItem>
+    </Grid>
+  ))}
+</Grid>
   );
+  
 };
 
 export default AttendanceCheck;
