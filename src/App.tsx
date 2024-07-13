5f604d9176e5b700e73cc37ea99a9054faf2a477
webLSG <166900725+webLSG@users.noreply.github.com>
2024-07-13 15:06:42 +0900

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Chip } from "@mui/material";
+import BasicDateCalendar from "./styles/Calendar";
+
 function App() {
-  return <div className="text-sky-300">hello 팀원들 test</div>;
+  return (
+    <div className="text-sky-300">
+      hello 팀원들
+      <Chip label="woo" />
+      <BasicDateCalendar />
+    </div>
+  );
 }
 
 export default App;
