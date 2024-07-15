9fbf4974bc1e0876946f1ac65cc46988e15de1ec
yokee <yokeekim@kakao.com>
2024-07-15 21:15:58 +0900

    yokeemainlayout

--- src/components/organism/Mainpage.tsx
+++ src/components/organism/Mainpage.tsx
@@ -7,9 +7,27 @@ const Mainpage = () => {
   return (
     <div className="w-screen h-screen my-auto mx-auto flex">
       <Layout />
-      <div className="w-4/5 my-4 mr-4 h-32 bg-gray-50 content-center text-center ">
-      <Navi/>
-      <BasicDateCalendar/>
+      <div className="w-4/5 my-4 mr-4 h-scree flex flex-col">
+        <div className="w-full h-32 bg-gray-50 content-center text-center">
+          <Navi />
+        </div>
+        <div className="w-full h-full flex">
+          <div className="w-3/5 h-full border m-2 border-red-500">
+            <div className="w-full h-1/2 p-3 flex">
+              <div className="w-1/2 h-full rounded-full border border-red-800 m-2"></div>
+              <div className="w-1/2 h-full border border-red-500"></div>
+            </div>
+            <div className="w-full h-1/2 p-3 border border-blue-400"></div>
+          </div>
+          <div className="w-2/5 h-screen-minus-128">
+            <div className="w-full h-1/2">
+              <BasicDateCalendar />
+            </div>
+            <div className="w-full h-1/2 border m-2 border-red-500">
+              <span>event</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
