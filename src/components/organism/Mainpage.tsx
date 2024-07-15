9c61ab2e78b2a88995239100ca74cda8c95ec4f4
yokeekim <yokee1128@naver.com>
2024-07-15 20:21:40 +0900

    yokee

--- src/components/organism/Mainpage.tsx
+++ src/components/organism/Mainpage.tsx
@@ -1,5 +1,7 @@
 import Navi from "../molecules/Navi";
 import Layout from "../layout/Layout";
+import { DateCalendar } from "@mui/x-date-pickers";
+import BasicDateCalendar from "../../styles/Calendar";
 
 const Mainpage = () => {
   return (
@@ -7,6 +9,7 @@ const Mainpage = () => {
       <Layout />
       <div className="w-4/5 my-4 mr-4 h-32 bg-gray-50 content-center text-center ">
       <Navi/>
+      <BasicDateCalendar/>
       </div>
     </div>
   );
