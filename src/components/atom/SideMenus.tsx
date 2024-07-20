8d548f8a10a45b3f0541198d4d75d2aae2051fb6
yokee <yokeekim@kakao.com>
2024-07-20 21:05:57 +0900

    20240720

--- src/components/atom/SideMenus.tsx
+++ src/components/atom/SideMenus.tsx
@@ -3,7 +3,7 @@ import { Chip } from "@mui/material";
 const SideMenus = () => {
   return (
     <div className="flex flex-col gap-5">
-      <Chip label="홈으로" component="a" href="mainpage" clickable />
+      <Chip label="홈으로" component="a" href="" clickable />
       <Chip label="출석체크" component="a" href="attendance_check" clickable />
       <Chip label="학생관리" component="a" href="students" clickable />
     </div>
