ba0f00607f47f729ae2041cf6302896d59d4cdd4
gsmkyj4862 <grand_knight@daum.net>
2024-08-06 21:38:02 +0900

    save

---
+++ src/components/atom/LogImg.tsx
@@ -0,0 +1,12 @@
+const LogImg = () => {
+  return (
+    <div className="w-14 h-14 rounded-full">
+      <img className="w-full h-full rounded-full"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIXhEupBZBns0PDN-dBgg3rkuawLMAGcHLgQ&s"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default LogImg;
